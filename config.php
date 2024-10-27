2359efb91ff22824afd9da314ab9b844c38ac062
vinifen <ferreiranivicius@gmail.com>
2024-10-27 16:49:15 -0300

    arrumar db dados

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
+define('DB_SERVER', 'db.multipass');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'demo');
+define('DB_PASSWORD', 'abc123');
+define('DB_NAME', 'company');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
